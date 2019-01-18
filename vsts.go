b923540b533ea9568ec620c09c54d45f9d2d965c
Mikael Krief <krief_mikael@hotmail.com>
2019-01-18 15:28:43 +0100

    fix

--- vsts.go
+++ vsts.go
@@ -73,7 +73,7 @@ func (s *Client) CreateProject(project Project) (string, error) {
 	var a [1]interface{}
 	a[0] = project
 	
-	projectBytes, _ := json.Marshal(a)
+	projectBytes, _ := json.Marshal(a[0])
 	projectReader := bytes.NewReader(projectBytes)
 	url := fmt.Sprintf(baseURL+"%s/_apis/projects?api-version=5.0-preview.3",s.organization)
 	log.Printf(url)

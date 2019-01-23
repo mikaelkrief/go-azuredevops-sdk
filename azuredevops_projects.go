f4a933c5a29657995a0abb884e5486b1e6f1e164
Mikael Krief <krief_mikael@hotmail.com>
2019-01-23 22:36:04 +0100

    update project

--- azuredevops_projects.go
+++ azuredevops_projects.go
@@ -28,13 +28,14 @@ func (s *Client) CreateProject(project Project) (string, error) {
 	return resp.Id, nil
 }
 
-func (s *Client) UpdateProject(project Project) (string, error) {
+
+func (s *Client) UpdateProject(projectid string, projectUpdated Project) (string, error) {
 	var a [1]interface{}
-	a[0] = project
+	a[0] = projectUpdated
 	
 	projectBytes, _ := json.Marshal(a[0])
 	projectReader := bytes.NewReader(projectBytes)
-	url := fmt.Sprintf(baseURL+"%s/_apis/projects/?api-version=5.0-preview.3",s.organization)
+	url := fmt.Sprintf(baseURL+"%s/_apis/projects/%s?api-version=5.0-preview.3",s.organization, projectid)
 	log.Printf(url)
 	req, err := http.NewRequest("PATCH", url, projectReader)
 	if err != nil {
